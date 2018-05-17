f628b1ada6de1825c1f050ae612cc1efd3d60429
Евгений Щербаков <Scherbacov1993evgeniy@yandex.ru>
2018-05-17 14:56:18 +0300

    add menu-acco & fix styles for it

--- app/js/module.js
+++ app/js/module.js
@@ -60,19 +60,19 @@ let verticalAccordeon = (function (options) {
 		}
 	};
 
-	_openItem = function _openItemNew(item, contentItem) {
+	let _openItem = function _openItem(item, contentItem) {
 		let contentHeight = _getHeight(contentItem);
 
 		item.classList.toggle(options.activeItemClass);
 		contentItem.style.height = contentHeight;
 	};
 
-	_closeItem = function _closeItem(item, contentItem) {
+	let _closeItem = function _closeItem(item, contentItem) {
 		contentItem.style.height = '';
 		item.classList.remove(options.activeItemClass);
 	};
 
-	_closeItems = function _closeItems(items) {
+	let _closeItems = function _closeItems(items) {
 		for (let i = 0; i < items.length; i++) {
 			let contentItem = items[i].querySelector('.accordeon__item-content');
 			_closeItem(items[i], contentItem);
@@ -93,3 +93,64 @@ let verticalAccordeon = (function (options) {
 });
 
 verticalAccordeon.init();
+
+/* Horizontal Accordeon */
+let horizontalAccordeon = (function (options) {
+	let list = document.querySelector(options.listSelector);
+	let itemsList = list.querySelectorAll('.menu-accordeon__item');
+
+	/* calculate needed width */
+	let userWidth = window.innerWidth;
+	let titleItem = list.querySelector('.'+options.linkClass);
+	let widthTitle = titleItem.clientWidth;
+	let neededWidth = userWidth - itemsList.length * widthTitle;
+	console.log(neededWidth);
+	neededWidth = (neededWidth > 520) ? 520 : neededWidth;
+
+	let _toogleItems = function (e) {
+		e.preventDefault();
+
+		if (e.target.className == options.linkClass) {
+			let item = e.target.parentNode;
+			let contentItem = item.querySelector('.menu-accordeon__item-content');
+
+			if (!item.classList.contains(options.activeItemClass)) {
+				_closeItems(itemsList);
+				_openItem(item, contentItem);
+			} else {
+				_closeItem(item, contentItem);
+			}
+		}
+	};
+
+	let _openItem = function _openItem(item, contentItem) {
+		item.classList.toggle(options.activeItemClass);
+		contentItem.style.width = neededWidth + 'px';
+	};
+
+	let _closeItem = function _closeItem(item, contentItem) {
+		contentItem.style.width = '';
+		item.classList.remove(options.activeItemClass);
+	};
+
+	let _closeItems = function _closeItems(items) {
+		for (let i = 0; i < items.length; i++) {
+			let contentItem = items[i].querySelector('.menu-accordeon__item-content');
+			_closeItem(items[i], contentItem);
+		}
+	};
+
+	let addListeners = function () {
+		list.addEventListener('click', _toogleItems)
+	};
+
+	return {
+		init: addListeners
+	}
+})({
+	listSelector: ".menu-accordeon__list",
+	linkClass: "menu-accordeon__trigger",
+	activeItemClass: "menu-accordeon__item--active"
+});
+
+horizontalAccordeon.init();
